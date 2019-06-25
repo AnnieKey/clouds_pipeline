896c0fc0f480fecad441c60d145a5a3895ce51c6
Anastasia Kovalyshyn <anastasiyakovalyshyn@gmail.com>
2019-06-25 12:05:07 +0300

    add new version and gitignore

--- modules/adapter_version.py
+++ modules/adapter_version.py
@@ -5,24 +5,18 @@ from azure.storage.blob import BlockBlobService
 from google.cloud import storage
 
 
-
 class Cloud_adaptee():
-    def __init__(self, file_name, bucket_name, file_name_in_cloud, file_name_after_downloaded, new_file_name, new_bucket_name):
-        self.file_name = file_name
-        self.bucket_name = bucket_name
-        self.file_name_in_cloud = file_name_in_cloud
-        self.file_name_after_downloaded = file_name_after_downloaded
-        self.new_file_name = new_file_name
-        self.new_bucket_name = new_bucket_name
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
     def create_file(self):
         import random
         tmp = [random.random() for _ in range(100)]
-        with open(self.file_name, 'w') as f:
+        with open(self.file_name, "w") as file:
             for i in range(len(tmp)):
-                f.write(str(i) + ":")
-                f.write(str(tmp[i]))
-                f.write(' ')
+                file.write(str(i) + ":")
+                file.write(str(tmp[i]))
+                file.write(' ')
 
     def wrap_data(self):
         import json
@@ -33,7 +27,9 @@ class Cloud_adaptee():
 
 
 class AWS_adaptee(Cloud_adaptee):
-    s3 = boto3.client('s3')
+    def __init__(self, **kwargs ):
+        Cloud_adaptee.__init__(self, **kwargs )
+        self.s3 = boto3.client("s3")
 
     def upload_file(self):
         self.s3.upload_file(self.file_name, self.bucket_name, self.file_name_in_cloud)
@@ -42,24 +38,28 @@ class AWS_adaptee(Cloud_adaptee):
         self.s3.download_file(self.bucket_name, self.file_name_in_cloud, self.file_name_after_downloaded)
 
     def rename_file(self):
-        self.s3.copy_object(Bucket=self.bucket_name, Key=self.new_file_name, CopySource=self.bucket_name + '/' + self.file_name)
+        self.s3.copy_object(Bucket=self.bucket_name, Key=self.new_file_name, CopySource=self.bucket_name + "/" + self.file_name)
         self.s3.delete_object(Bucket=self.bucket_name, Key=self.file_name)
 
     def move_file(self):
-        self.s3.copy_object(Bucket=self.new_bucket_name, Key=self.file_name, CopySource=self.bucket_name + '/' + self.file_name)
+        self.s3.copy_object(Bucket=self.new_bucket_name, Key=self.file_name, CopySource=self.bucket_name + "/" + self.file_name)
         self.s3.delete_object(Bucket=self.bucket_name, Key=self.file_name)
 
 
 class Azure_adaptee(Cloud_adaptee):
+    def __init__(self, **kwargs ):
+        Cloud_adaptee.__init__(self, **kwargs )
+        self.block_blob_service = BlockBlobService()
+
     def upload_file(self):
-        full_path_to_file = '/home/anniekey/Projects/cloud_pipeline/' + self.file_name
+        full_path_to_file = "/home/anniekey/Projects/cloud_pipeline/" + self.file_name
         self.block_blob_service.create_blob_from_path(self.bucket_name, self.file_name, full_path_to_file)
 
     def download_file(self):
         self.block_blob_service.get_blob_to_path(self.bucket_name, self.file_name_in_cloud, self.file_name_after_downloaded)
 
     def rename_file(self):
-        file_name = 'files/' + self.file_name
+        file_name = "files/" + self.file_name
         blob_url = self.block_blob_service.make_blob_url(self.bucket_name, file_name)
         self.block_blob_service.copy_blob(self.bucket_name, self.new_file_name, blob_url)
         self.block_blob_service.delete_blob(self.bucket_name, self.file_name)
@@ -71,7 +71,9 @@ class Azure_adaptee(Cloud_adaptee):
 
 
 class Google_adaptee(Cloud_adaptee):
-    storage_client = storage.Client()
+    def __init__(self, **kwargs):
+        Cloud_adaptee.__init__(self, **kwargs)
+        self.storage_client = storage.Client()
 
     def upload_file(self):
         bucket = self.storage_client.get_bucket(self.bucket_name)
@@ -106,8 +108,8 @@ class Target_interface(ABC):
 
 
 class Adapter(Target_interface, AWS_adaptee, Azure_adaptee, Google_adaptee):
-    def __init__(self, adaptee,file_name, bucket_name, file_name_in_cloud, file_name_after_downloaded, new_file_name, new_bucket_name ):
-        Cloud_adaptee.__init__(self,file_name, bucket_name, file_name_in_cloud, file_name_after_downloaded, new_file_name, new_bucket_name )
+    def __init__(self, adaptee, **kwargs ):
+        Cloud_adaptee.__init__(self, **kwargs )
         self.adaptee = adaptee
 
     def create_file(self):
@@ -129,23 +131,46 @@ class Adapter(Target_interface, AWS_adaptee, Azure_adaptee, Google_adaptee):
         self.adaptee.move_file()
 
 
-def client_code(adapter):
-    adapter.create_file()
-    adapter.upload_file()
-    adapter.download_file()
-    adapter.wrap_data()
-    adapter.rename_file()
-    adapter.move_file()
+class Client:
+    def __init__(self, adapter):
+        self.adapter = adapter
+
+    def create_file(self):
+        self.adapter.create_file()
+
+    def upload_file(self):
+        self.adapter.upload_file()
+
+    def download_file(self):
+        self.adapter.download_file()
+
+    def wrap_data(self):
+        self.adapter.wrap_data()
+
+    def rename_file(self):
+        self.adapter.rename_file()
+
+    def move_file(self):
+        self.adapter.move_file()
 
 
 def main():
-    bucket_name = 'testcloudanniekey'
-    new_bucket_name = 'testmovement'
-    file_name = "files/created_file.txt"
-    file_name_in_cloud = 'created_file.txt'
-    new_file_name = 'renamed_file.txt'
-    file_name_after_downloaded = 'files/downloaded_file.txt'
+    bucket_name = "testcloudanniekey"
+    new_bucket_name = "testmovement"
+    file_name = "/home/anniekey/Projects/cloud_pipeline/files/test.txt"
+    file_name_in_cloud = "created_file.txt"
+    new_file_name = "renamed_file.txt"
+    file_name_after_downloaded = "/home/anniekey/Projects/cloud_pipeline/files/downloaded_file.txt"
 
-    adaptee = AWS_adaptee(file_name, bucket_name, file_name_in_cloud, file_name_after_downloaded, new_file_name, new_bucket_name)
-    adapter = Adapter(adaptee, file_name, bucket_name, file_name_in_cloud, file_name_after_downloaded, new_file_name, new_bucket_name)
-    client_code(adapter)
+    adaptee = AWS_adaptee(file_name=file_name,
+                      bucket_name=bucket_name,
+                      file_name_in_cloud=file_name_in_cloud,
+                      file_name_after_downloaded=file_name_after_downloaded,
+                      new_file_name=new_file_name,
+                      new_bucket_name=new_bucket_name)
+    adapter = Adapter(adaptee)
+    client = Client(adapter)
+
+
+if __name__ == "__main__":
+    main()
